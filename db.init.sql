735a2bd9012f39411697145f3c986cd86fa81768
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-02-12 20:32:47 +0530

    Update init for user table

--- db.init.sql
+++ db.init.sql
@@ -1,12 +1,14 @@
 create database if not exists sankalan;
 use sankalan;
 
-create table user (
-    ts int(10) not null,
-    id char(21) not null primary key,
-    name varchar(40) not null,
-    email varchar(32) not null,
-    -- picture varchar(92),
-    org varchar(60),
-    hash char(32) not null unique
+CREATE TABLE `user` (
+ `ts` int(10) NOT NULL,
+ `id` char(21) NOT NULL,
+ `name` varchar(40) NOT NULL,
+ `email` varchar(32) NOT NULL,
+ `mobile` char(10) NOT NULL,
+ `org` varchar(80) DEFAULT NULL,
+ `hash` char(32) NOT NULL,
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `hash` (`hash`)
 );
