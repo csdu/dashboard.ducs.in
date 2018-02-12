5469e66deebfc0773d836624bff4c2d418fc4f33
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-02-12 15:55:51 +0530

    Update gulp tasks for prod and dev builds Add script to convert json asset manifest to php array Add eslint for js assets

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,29 @@ const sass = require('gulp-sass');
 const uglifyJs = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const clean = require('gulp-clean');
-const rename = require('gulp-rename');
 const hash = require('gulp-hash');
 
 const srcDir = path.join(process.cwd(), './src-assets');
 const distDir = path.join(process.cwd(), './assets');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const paths = {
   css: {
     src: `${srcDir}/css/**/*.css`,
-    dest: `${distDir}/css`
+    dest: `${distDir}/css`,
   },
   sass: {
     src: `${srcDir}/sass/**/*.sass`,
-    dest: `${distDir}/css`
+    dest: `${distDir}/css`,
   },
   js: {
     src: [`${srcDir}/js/**/*.js`, `!${srcDir}/js/**/*.min.js`],
-    dest: `${distDir}/js`
+    dest: `${distDir}/js`,
   },
   jsCopy: {
     src: `${srcDir}/js/**/*.min.js`,
-    dest: `${distDir}/js`
+    dest: `${distDir}/js`,
   },
   assetManifest: path.join(process.cwd(), '/src/templates/assets.json'),
 };
@@ -45,12 +46,9 @@ const options = {
   sass: {
     outputStyle: 'compressed',
   },
-  rename: {
-    suffix: '.min'
-  },
   hash: {
     hash: {
-      hashLength: 12,
+      hashLength: 6,
     },
     js: {
       deleteOld: true,
@@ -65,56 +63,87 @@ const options = {
 
 const cleanDist = () =>
   gulp.src(distDir, {
-    read: false
+    read: false,
   })
-  .pipe(clean());
+    .pipe(clean());
 
-const minifyJs = () =>
-  gulp.src(paths.js.src, {
-    since: gulp.lastRun(minifyJs)
+// minify and copy js
+const minifyJs = () => {
+  if (isProduction) {
+    return gulp.src(paths.js.src)
+      .pipe(sourcemaps.init())
+      .pipe(hash(options.hash.hash))
+      .pipe(babel(options.babel))
+      .pipe(uglifyJs(options.uglifyJs))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(paths.js.dest))
+      .pipe(hash.manifest(paths.assetManifest, options.hash.js))
+      .pipe(gulp.dest('.'));
+  }
+  return gulp.src(paths.js.src, {
+    since: gulp.lastRun(minifyJs),
   })
-  .pipe(sourcemaps.init())
-  .pipe(hash(options.hash.hash))
-  .pipe(babel(options.babel))
-  .pipe(uglifyJs(options.uglifyJs))
-  .pipe(rename(options.rename))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest(paths.js.dest))
-  .pipe(hash.manifest(paths.assetManifest, options.hash.js))
-  .pipe(gulp.dest('.'));
+    .pipe(hash(options.hash.hash))
+    .pipe(gulp.dest(paths.js.dest))
+    .pipe(hash.manifest(paths.assetManifest, options.hash.js))
+    .pipe(gulp.dest('.'));
+};
 
-const copyJs = () =>
-  gulp.src(paths.jsCopy.src, {
-    since: gulp.lastRun(copyJs)
+// copy js directly
+const copyJs = () => {
+  if (isProduction) {
+    return gulp.src(paths.jsCopy.src)
+      .pipe(hash(options.hash.hash))
+      .pipe(gulp.dest(paths.jsCopy.dest))
+      .pipe(hash.manifest(paths.assetManifest, options.hash.js))
+      .pipe(gulp.dest('.'));
+  }
+  return gulp.src(paths.jsCopy.src, {
+    since: gulp.lastRun(copyJs),
   })
-  .pipe(hash(options.hash.hash))
-  .pipe(gulp.dest(paths.jsCopy.dest))
-  .pipe(hash.manifest(paths.assetManifest, options.hash.js))
-  .pipe(gulp.dest('.'));
+    .pipe(hash(options.hash.hash))
+    .pipe(gulp.dest(paths.js.dest))
+    .pipe(hash.manifest(paths.assetManifest, options.hash.js))
+    .pipe(gulp.dest('.'));
+};
 
-const copyCss = () =>
-  gulp.src(paths.css.src, {
-    since: gulp.lastRun(copyCss)
+const copyCss = () => {
+  if (isProduction) {
+    return gulp.src(paths.css.src)
+      .pipe(hash(options.hash.hash))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest(paths.css.dest))
+      .pipe(hash.manifest(paths.assetManifest, options.hash.css))
+      .pipe(gulp.dest('.'));
+  }
+  return gulp.src(paths.css.src, {
+    since: gulp.lastRun(copyCss),
   })
-  .pipe(hash(options.hash.hash))
-  .pipe(cleanCSS())
-  .pipe(gulp.dest(paths.css.dest))
-  .pipe(hash.manifest(paths.assetManifest, options.hash.css))
-  .pipe(gulp.dest('.'));
+    .pipe(hash(options.hash.hash))
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(hash.manifest(paths.assetManifest, options.hash.css))
+    .pipe(gulp.dest('.'));
+};
 
-const compileSass = () =>
-  gulp.src(paths.sass.src, {
-    since: gulp.lastRun(compileSass)
+const compileSass = () => {
+  if (isProduction) {
+    return gulp.src(paths.sass.src)
+      .pipe(hash(options.hash.hash))
+      .pipe(sass(options.sass).on('error', sass.logError))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest(paths.sass.dest))
+      .pipe(hash.manifest(paths.assetManifest, options.hash.css))
+      .pipe(gulp.dest('.'));
+  }
+  return gulp.src(paths.sass.src, {
+    since: gulp.lastRun(compileSass),
   })
-  .pipe(hash(options.hash.hash))
-  .pipe(sass(options.sass).on('error', sass.logError))
-  .pipe(cleanCSS())
-  .pipe(rename({
-    suffix: '.min'
-  }))
-  .pipe(gulp.dest(paths.sass.dest))
-  .pipe(hash.manifest(paths.assetManifest, options.hash.css))
-  .pipe(gulp.dest('.'));
+    .pipe(sass(options.sass).on('error', sass.logError))
+    .pipe(hash(options.hash.hash))
+    .pipe(gulp.dest(paths.sass.dest))
+    .pipe(hash.manifest(paths.assetManifest, options.hash.css))
+    .pipe(gulp.dest('.'));
+};
 
 const build = gulp.parallel(
   minifyJs,
@@ -128,7 +157,7 @@ const watch = () => {
   gulp.watch(paths.css.src, copyCss);
   gulp.watch(paths.js.src, minifyJs);
   gulp.watch(paths.jsCopy.src, copyJs);
-}
+};
 
 module.exports = {
   default: build,
---
+++ scripts/assetsJsonToPhp.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+
+const assetManifest = {
+  json: path.join(process.cwd(), '/src/templates/assets.json'),
+  php: path.join(process.cwd(), '/src/templates/assets.php'),
+}
+
+fs.readFile(assetManifest.json, 'utf-8', (err, jsonString) => {
+  const json = JSON.parse(jsonString);
+
+  let phpStr = '<?php \n$assets = [\n';
+  for (const key in json) {
+    phpStr += `    '${key}' => '${json[key]}',\n`
+  }
+  phpStr += '];'
+
+  fs.writeFile(assetManifest.php, phpStr, 'utf-8', console.log);
+});
