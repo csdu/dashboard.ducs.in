894dcdd6d522e2b84b20d47c5c191cd3bba7f2af
Manish Arora <manish_arora@outlook.com>
2018-03-25 23:12:10 +0530

    Questions can now be retrieved for specified event_id through quiz/get-problems/{id}

--- src/app/Sankalan/Quiz/Quiz.php
+++ src/app/Sankalan/Quiz/Quiz.php
@@ -21,29 +21,37 @@ class Quiz extends Router
     {
         $uri = $this->uri;
 
-        switch ($uri) {
-            case '':
-                $this->res->setContent('quiz home');
-                break;
-
-            case '/get-problems':
-                include 'get-problems.php';
-                $event = 'algoholics'; // figure out event name
-                $this->res = new JsonResponse(getProblems($event));
-                break;
-
-            default:
-                $event;
-                if (in_array($uri, $this->events)) {
-                    $event = $uri;
-                }
-                if (!isset($event)) {
-                    $this->res->setStatusCode(404);
-                    return;
-                }
-                $this->res->setContent('quiz page for : [' . $uri . ']');
-                break;
+        if (startsWith($uri, '/get-problems')) {
+            include 'get-problems.php';
+            $args = explode("/", $uri); // figure out event name
+            if (isset($args[2])) {
+                $this->res = new JsonResponse(getProblems($args[2]));
+            }
+            // else {
+            //     // return 400 status
+            // }
         }
-
+        else if ($uri === '/submit' and $this->req->isMethod('POST')) {
+            $this->res->setContent('quiz home');
+        }
+        else if ($uri === '/respond' and $this->req->isMethod('POST')) {
+            include 'save-response.php';
+            
+            $this->res->setContent('quiz home');
+        }
+        else if ($uri === '') {
+            $this->res->setContent('quiz home');
+        } 
+         else {
+            $event;
+            if (in_array($uri, $this->events)) {
+                $event = $uri;
+            }
+            if (!isset($event)) {
+                $this->res->setStatusCode(404);
+                return;
+            }
+            $this->res->setContent('quiz page for : [' . $uri . ']');
+        }
     }
 }
--- src/app/Sankalan/Quiz/get-problems.php
+++ src/app/Sankalan/Quiz/get-problems.php
@@ -1,14 +1,23 @@
 <?php
 
+use \DUCS\Sankalan\Database;
+
 /**
  * returns problems json based on event
  * @param  string $event event id
  * @return array        list of problems
  */
-function getProblems($event)
+function getProblems($event_id)
 {
+    $db = new Database();
+    $event = $db->query('SELECT name FROM events WHERE event_id = :event_id', ['event_id' => $event_id], true);
+
+    if (!$event) die("Invalid ID");
+
+    $problems = $db->query('SELECT qid, statement, choice1, choice2, choice3, choice4, multi_sol FROM questions WHERE event_id = :event_id LIMIT 30', ['event_id' => $event_id], false);
+
     return [
         'event' => $event,
-        'problems' => [],
+        'problems' => $problems,
     ];
 }
